6f279f4d7008ef166e2678d1d0ef90db849fa740
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:39:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,27 @@
 import streamlit as st
-if 'button' not in st.session_state:
-    st.session_state.button = False
 
-def click_button():
-    st.session_state.button = not st.session_state.button
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
 
-st.button('Click me', on_click=click_button)
+def set_state(i):
+    st.session_state.stage = i
 
-if st.session_state.button:
-    # The message and nested widget will remain on the page
-    st.write('Button is on!')
-    st.slider('Select a value')
-else:
-    st.write('Button is off!')
+if st.session_state.stage == 0:
+    st.button('Begin', on_click=set_state, args=[1])
+
+if st.session_state.stage >= 1:
+    name = st.text_input('Name', on_change=set_state, args=[2])
+
+if st.session_state.stage >= 2:
+    st.write(f'Hello {name}!')
+    color = st.selectbox(
+        'Pick a Color',
+        [None, 'red', 'orange', 'green', 'blue', 'violet'],
+        on_change=set_state, args=[3]
+    )
+    if color is None:
+        set_state(2)
+
+if st.session_state.stage >= 3:
+    st.write(f':{color}[Thank you!]')
+    st.button('Start Over', on_click=set_state, args=[0])
