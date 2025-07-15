76f6bee71ef44d77e7839682e2e0968a83327de8
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 11:03:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ your-repository/
 │   ├── page_1.py
 │   └── page_2.py
 └── your_app.py
+  import streamlit as st
 st.page_link("your_app.py", label="Home", icon="🏠")
 st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
 st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
