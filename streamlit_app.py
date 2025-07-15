c2150ce96391cb32f97e3c6c90e3a317966bad19
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:30:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
+
 def show_materi(topik):
             if topik=="Kimia Organik":
-                    st.subheader("Materi Kimia Organik")
-                    st.write("""
-                    - Hidrokarbon
-                    - Alkohol Fenol Eter
-                    - Aldehid dan Keton
-                    - Asam Kerboksilat
-                    - Amina
-                    """)
+                        st.subheader("Materi Kimia Organik")
+                        st.write("""
+                        - Hidrokarbon
+                        - Alkohol Fenol Eter
+                        - Aldehid dan Keton
+                        - Asam Kerboksilat
+                        - Amina
+                        """)
             else:
                         st.subheader("Materi Kimia Anorganik")
                         st.write("""
