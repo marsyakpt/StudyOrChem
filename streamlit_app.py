a3effd0eb73b8bd84859132e42e0fbbee0ec254f
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:37:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
 
-animal = st.text_input('Type an animal')
+def click_button():
+    st.session_state.clicked = True
 
-if st.button('Check availability'):
-    have_it = animal.lower() in animal_shelter
-    'We have that animal!' if have_it else 'We don\'t have that animal.'
+st.button('Click me', on_click=click_button)
+
+if st.session_state.clicked:
+    # The message and nested widget will remain on the page
+    st.write('Button clicked!')
+    st.slider('Select a value')
