124cb5b03d1bd369828f0f23e8c82e49c25281ea
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:38:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
-
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
+if 'button' not in st.session_state:
+    st.session_state.button = False
 
 def click_button():
-    st.session_state.clicked = True
+    st.session_state.button = not st.session_state.button
 
 st.button('Click me', on_click=click_button)
 
-if st.session_state.clicked:
+if st.session_state.button:
     # The message and nested widget will remain on the page
-    st.write('Button clicked!')
+    st.write('Button is on!')
     st.slider('Select a value')
+else:
+    st.write('Button is off!')
