e33001b12a6d467b4a11a69137dc68ed498df5bf
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:45:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,14 @@
 import streamlit as st
-from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+import time
 
-@dataclass
-class MyDataclass:
-    var1: int
-    var2: float
+df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
+my_data_element = st.line_chart(df)
 
-    def to_str(self):
-        return f"{self.var1},{self.var2}"
+for tick in range(10):
+    time.sleep(.5)
+    add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+    my_data_element.add_rows(add_df)
 
-    @classmethod
-    def from_str(cls, serial_str):
-        values = serial_str.split(",")
-        var1 = int(values[0])
-        var2 = float(values[1])
-        return cls(var1, var2)
-
-if "my_dataclass" not in st.session_state:
-    st.session_state.my_dataclass = MyDataclass(1, 5.5).to_str()
-
-# Displays True on every rerun
-MyDataclass.from_str(st.session_state.my_dataclass) == MyDataclass(1, 5.5)
-
-st.button("Rerun")
+st.button("Regenerate")
