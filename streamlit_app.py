9a17190b68bbc23251adfdc571ed6930e36bf8bf
marsyakpt <mrsyakrtika112@gmail.com>
2025-07-15 10:40:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,16 @@
 import streamlit as st
 
-if 'stage' not in st.session_state:
-    st.session_state.stage = 0
+import pandas as pd
 
-def set_state(i):
-    st.session_state.stage = i
+if 'name' not in st.session_state:
+    st.session_state['name'] = 'John Doe'
 
-if st.session_state.stage == 0:
-    st.button('Begin', on_click=set_state, args=[1])
+st.header(st.session_state['name'])
 
-if st.session_state.stage >= 1:
-    name = st.text_input('Name', on_change=set_state, args=[2])
+if st.button('Jane'):
+    st.session_state['name'] = 'Jane Doe'
 
-if st.session_state.stage >= 2:
-    st.write(f'Hello {name}!')
-    color = st.selectbox(
-        'Pick a Color',
-        [None, 'red', 'orange', 'green', 'blue', 'violet'],
-        on_change=set_state, args=[3]
-    )
-    if color is None:
-        set_state(2)
+if st.button('John'):
+    st.session_state['name'] = 'John Doe'
 
-if st.session_state.stage >= 3:
-    st.write(f':{color}[Thank you!]')
-    st.button('Start Over', on_click=set_state, args=[0])
+st.header(st.session_state['name'])
